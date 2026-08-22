01fff535ab3f8daf87e93e233a86074ccb7e2223
cchithramary-cell <cchithramary@gmail.com>
2026-08-22 12:54:50 +0530

    final_changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,18 @@
 const mysql = require("mysql2");
 const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 
   ssl: {
-    ca: fs.readFileSync(process.env.DB_SSL_CA),
+    ca: fs.readFileSync(path.join(__dirname, "../ca.pem")),
   },
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-module.exports = connection.promise();
+module.exports = pool.promise();
